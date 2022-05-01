a2a9334c04f7e1c42bd19a29c22f29646f883650
DanilAndreev <danssg08@gmail.com>
2022-05-01 16:16:12 +0300

    [CurveMorphingTool] Cleanup

--- src/tools/shaders/bin/GLSLShaders.h
+++
@@ -1,2 +0,0 @@
-#include "include\GLSL\image_morphing_tool.frag.glsl.h"
-#include "include\GLSL\image_morphing_tool.vert.glsl.h"
--- src/tools/shaders/bin/SPIRVShaders.h
+++
@@ -1,2 +0,0 @@
-#include "include\SPIRV\image_morphing_tool.frag.spv.h"
-#include "include\SPIRV\image_morphing_tool.vert.spv.h"
